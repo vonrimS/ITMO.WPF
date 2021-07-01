579336e8337e89d3b179523029c73c24be537696
vonrimS <mr.mytab@gmail.com>
2021-07-01 18:37:26 +0300

    Praxis08 Exercise01

---
+++ Praxis08 Exercise01/MyWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Praxis08_Exercise01
+{
+    /// <summary>
+    /// Interaction logic for MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+
+        private bool _close;
+        MainWindow wnd1 = null;
+
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+
+        public new void Close()
+        { 
+            _close = true;
+            base.Close(); 
+        }
+
+        public void PrintLogFile()
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("log.txt", true))
+            {
+                writer.WriteLine("Внесено {0}: {1} ", textBox.Text, DateTime.Now.ToShortDateString() + ", время: " +
+                DateTime.Now.ToLongTimeString());
+                writer.Flush();
+            }
+        }       
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_close) return; 
+            e.Cancel = true; 
+            Hide();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wnd1 = Owner as MainWindow;
+            if (wnd1 != null)
+            {
+                wnd1.txtBlock.Text = textBox.Text;
+                PrintLogFile();
+            }
+            Close();
+
+            
+            
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            wnd1.myWin = null;
+        }
+    }
+}
---
+++ Praxis08 Exercise01/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Praxis08_Exercise01
+{
+    public class Student
+    {
+
+        public string StudentName { get; set; }
+
+        public bool IsEnrolled { get; set; }
+
+        public Student(string name, bool ch)
+        {
+            StudentName = name;
+            IsEnrolled = ch;
+        }
+        public string FullStudentData
+        {
+            get { return StudentName + "\t" + IsEnrolled; }
+        }
+        // или тоже самое, но с помощью синтаксиса лямбда выражения:
+        // public string FullStudentData => StudentName + "\t" + IsEnrolled;
+    }
+
+    public class StudentList : ObservableCollection<Student>
+    {
+        public StudentList() : base()
+        {
+            Add(new Student("Lorin Kanev", true));
+            Add(new Student("Ivan Petrov", true));
+            Add(new Student("Sergey Masov", false));
+            Add(new Student("Tais Frolova", true));
+            Add(new Student("Elena Diva", false));
+        }
+    }
+}
+
